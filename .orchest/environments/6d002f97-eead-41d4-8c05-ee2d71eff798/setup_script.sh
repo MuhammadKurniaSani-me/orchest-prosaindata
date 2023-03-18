5bfc22a88f2729dae53c94cbf7c3508f5f843d4c
MuhammadKurniaSani-me <sani230402@gmail.com>
2023-03-18 06:08:26 +0000

    prosaindata-tugas-3

---
+++ .orchest/environments/6d002f97-eead-41d4-8c05-ee2d71eff798/setup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install any dependencies you have in this shell script,
+# see https://docs.orchest.io/en/latest/fundamentals/environments.html#install-packages
+
+pip install jupyterlab_code_formatter
+
+pip install pandas scikit-learn psycopg2-binary 
+
+# for ORM sqlalchemy
+pip install typing 
+pip install SQLAlchemy
+pip install typing
+pip install mysql-connector-python
+pip install PyMySQL
+# pip install -r requirements.txt
+# conda install tensorflow
+# sudo apt-get install graphviz
